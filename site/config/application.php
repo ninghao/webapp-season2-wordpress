0771b065893697ae1a5843c52850bfc2861e755d
wanghao8080 <117663444@qq.com>
2017-12-04 11:54:50 +0800

    在环境变量中存储小程序 ID 与 密钥

--- site/config/application.php
+++ site/config/application.php
@@ -17,7 +17,7 @@ Env::init();
 $dotenv = new Dotenv\Dotenv($root_dir);
 if (file_exists($root_dir . '/.env')) {
     $dotenv->load();
-    $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
+    $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL', 'WX_APP_ID', 'WX_APP_SECRET']);
 }
 
 /**
